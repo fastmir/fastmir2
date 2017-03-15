af4d2ae21f90ec3dd4c95557531110a1601e9fef
fastmir <parminho@hanmail.net>
2017-03-15 17:25:09 +0900

    nodemon

---
+++ test2.js
@@ -0,0 +1,45 @@
+<!DOCCTYPE html>
+<html>
+<head>
+	<title>CSS3 selector Basic Page</title>
+
+	<style>
+	#header {
+		width: 800px; margin: 0 auto;
+		background: red;
+	}
+	#wrap {
+		width: 800px; margin: 0 auto;
+	}
+	#aside {
+		width:200px; float: left;
+		background: blue;
+	}
+	#content {
+		width:600px; float: left;
+		background: green;
+	}
+	</style>
+
+</head>
+<body>
+	<div id="header">
+	<h1>header</h1>
+	</div>
+
+	<div id="wrap">
+		<div id="aside">
+		<h1>aside</h1>
+		</div>
+		<div id="content">
+		<h1>content</h1>
+		</div>
+	</div>
+
+<br>
+<h1>aa</h1>
+<p>bbb</p>
+
+
+</body>
+</html>
