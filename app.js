4e1cb716dd1fc90ca5f84771422794c5b981964b
fastmir <parminho@hanmail.net>
2017-03-15 18:21:36 +0900

    install

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
-console.log(__dirname);
+app.get('/',function (req,res){
+  res.send('hello world!');
+});
 
 app.listen(3000, function(){
-  console.log('server on!');
+  console.log('server On!');
 });
